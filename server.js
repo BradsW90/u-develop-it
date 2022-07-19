ccb8816fb55e5a8d14e01162426b9a1b52fbca13
Amanda Hefner <bswj90@gmail.com>
2022-07-19 19:45:14 -0400

    made several calls to database

--- server.js
+++ server.js
@@ -24,8 +24,33 @@ app.get("/", (req, res) => {
   });
 });
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
+//db.query(`SELECT * FROM candidates`, (err, rows) => {
+//  console.log(rows);
+//});
+//Get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+//DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+//Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+VALUES (?,?,?,?)`;
+const params = [1, "Ronald", "Firbank", 1];
+
+db.query(sql, params, (err, results) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(results);
 });
 
 app.use((req, res) => {
