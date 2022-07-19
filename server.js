1bf2a2be163c26eca003fc7c8b04f0c6bb4d3f17
Amanda Hefner <bswj90@gmail.com>
2022-07-19 19:13:53 -0400

    linked database to server

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require("express");
+const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+//Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    //Your MySQL username
+    user: "root",
+    //Your MySQL password
+    password: "D@t@b@$3Acce$$",
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
 
 app.get("/", (req, res) => {
   res.json({
     mesage: "Hello World",
   });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
 });
 
 app.use((req, res) => {
